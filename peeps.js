c6da1e9d362a0d0a6d9b848c4bd69c8a18915a12
Daniel <lekeayomidotun@gmail.com>
2023-10-05 13:05:38 +0200

    Folder re-arranged all links with files

--- peeps.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#yes").on("click", function () {
-        $("#yes").fadeOut(1000);
-        $("#no").fadeOut(1000)
-        $(".hidden").css({display : "block"}).fadeIn(1000);
-      });
-    });
